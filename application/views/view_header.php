e38ae4911b72414272f8d8a34b6d93dddbf7ee6f
Hanskz Hernández Díaz <h4nskz10@gmail.com>
2017-03-27 14:55:57 -0500

    Initial Commit

---
+++ application/views/view_header.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="POS(Punto de venta)">
+  <meta name="author" content="Ing. Manuel Cortes Crisanto">
+  <link rel="icon" type="image/<?php echo EXTENSION_IMAGEN_FAVICON; ?>" href="<?php echo base_url()?>img/<?php echo NOMBRE_IMAGEN_FAVICON; ?>" />
+
+  <title><?php echo TITULO_PAGINA; ?></title>  
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/skins/_all-skins.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/iCheck/flat/blue.css">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/morris/morris.css">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/datepicker/datepicker3.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/daterangepicker/daterangepicker.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b><?php echo NOMBRE_EMPRESA_MINI; ?></b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b><?php echo NOMBRE_EMPRESA; ?></b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown messages-menu">
+            <a href="#" class="dropdown-toggle">
+              <strong>Bienvenido: </strong>
+            </a>            
+          </li>          
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <span class="hidden-xs"><?php echo $this->session->userdata('NOMBRE').' '.$this->session->userdata('APELLIDOS'); ?></span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="<?php echo base_url() ?>assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <p>
+                  <?php echo $this->session->userdata('NOMBRE').' '.$this->session->userdata('APELLIDOS'); ?><br />
+                  <?php echo '('.$this->session->userdata('TIPOUSUARIOMS').')'; ?>
+                </p>
+              </li>              
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Perfil</a>
+                </div>
+                <div class="pull-right">
+                  <a href="<?php echo base_url().'login/CerrarSesion'; ?>" class="btn btn-default btn-flat">Salir</a>
+                </div>
+              </li>
+            </ul>
+          </li>          
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p><?php echo $this->session->userdata('NOMBRE').' '.$this->session->userdata('APELLIDOS'); ?></p>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+              <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-folder"></i> <span>Catálogos</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="pages/examples/invoice.html"><i class="fa fa-circle-o"></i> Productos</a></li>
+            <li><a href="pages/examples/profile.html"><i class="fa fa-circle-o"></i> Clientes</a></li>
+            <li><a href="pages/examples/login.html"><i class="fa fa-circle-o"></i> Proveedores</a></li>
+            <li><a href="pages/examples/register.html"><i class="fa fa-circle-o"></i> Categorías</a></li>
+            <li><a href="pages/examples/lockscreen.html"><i class="fa fa-circle-o"></i> Usuarios</a></li>            
+          </ul>
+        </li>       
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Ventas</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> Nueva venta</a></li>            
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-table"></i> <span>Compras</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/tables/simple.html"><i class="fa fa-circle-o"></i> Orden de compra</a></li>            
+          </ul>
+        </li>        
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-pie-chart"></i>
+            <span>Reportes</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> Reporte de movimientos</a></li>            
+          </ul>
+        </li>         
+      </ul>
+
+
+
+      <div id="treeMenu"><br />        
+        <ul>
+          <?php
+            $contador  = 0;
+            $LineaTemp = 0;
+            $IdMenu    = 0;
+            session_start();
+            $ArrayMenu = $_SESSION['Menu'];
+            foreach ($ArrayMenu as $key => $value) 
+            {
+              $linea    = $value->Linea;
+              $url      = $value->URL;
+              $IdMenu   = $value->IdMenu;
+              if($linea==1)
+              {
+                $LineaTemp = $value->IdMenu;
+                echo '<li>';
+                echo '<a href="#" class="parent">'.$value->Descripcion.'</a><span></span>';
+                echo '<div>';
+                echo '<ul>';
+              }
+              
+              if($linea == $LineaTemp)
+              { 
+                echo '<li><span></span><a href="'.base_url().$url.'">'.$value->Descripcion.'</a></li>';
+              }
+
+              if($url == "usuarios" or $url == "ventas" or $url == "ordencompra" or $url == "reportes")
+              {
+                echo '</ul>';
+                echo '</div>';
+                echo '</li>'; 
+              }
+            }
+          ?>
+        </ul><hr /><br />
+      </div>
+    
+      
+
+
+
+
+
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
